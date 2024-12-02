39efcc83f12b96f52959ee02ca47996c3e7b35cf
Dominaaaaa <kfirman253@gmail.com>
2024-12-02 20:09:31 +0700

    Upload all project files

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,17 @@
+<?php
+// app/Models/Mahasiswa.php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nim',
+        'nama',
+        'jurusan'
+    ];
+}
